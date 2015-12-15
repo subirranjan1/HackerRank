f5d4264914670e5ec45ff476bd0b732091ac34ad
subirranjan1 <subir.ranjan1@gmail.com>
2015-12-14 19:52:52 -0800

    hackerrank initial1

---
+++ HackerRank/src/BigInteger.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.math.*;
+
+public class BigInteger {
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases=in.nextInt();
+	}
+}
---
+++ HackerRank/src/PlusMinus.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class PlusMinus {
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int R = in.nextInt();
+            int C = in.nextInt();
+            String G[] = new String[R];
+            for(int G_i=0; G_i < R; G_i++){
+                G[G_i] = in.next();
+            }
+            int r = in.nextInt();
+            int c = in.nextInt();
+            String P[] = new String[r];
+            for(int P_i=0; P_i < r; P_i++){
+                P[P_i] = in.next();
+            }
+            for(int i=0;i<R;i++){
+                for (int j=0;j<r;j++){
+                    String grid=G[i].toString();
+                    String pattern=P[j].toString();
+                    boolean match=grid.toLowerCase().contains(pattern.toLowerCase());
+                    if(match){
+                    	System.out.println(grid);
+                    	System.out.println(pattern);
+                    }
+                }           
+            }
+        }
+	}
+}
